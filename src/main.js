cb8cd10615b00da01a47cd578bd0d8131f020e27
Ryan Putman <putman10@me.com>
2018-08-10 11:32:58 -0700

    add extra classes and styling to seperate looped results

--- src/main.js
+++ src/main.js
@@ -27,19 +27,19 @@ $(document).ready(function() {
         $("#results").append("<div class='col-md-3 doctor-box'id=doctor" + counter + "></div>");
         $("#doctor" + counter).append("<img src='" + doctor.profile.image_url + "' alt='doctorimage' >");
         $("#doctor" + counter).append("<h3 class='name'>" + doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title + "</h3>");
-        $("#doctor" + counter).append("<div id='language" + counter + "'></div>");
+        $("#doctor" + counter).append("<p class='description'>" + doctor.specialties["0"].description + "</p>");
+        $("#doctor" + counter).append("<div id='language" + counter + "'><p class='bold'>Languages:</p></div>");
         for (let i = 0; i < doctor.profile.languages.length; i++) {
           $("#language" + counter).append("<p>" + doctor.profile.languages[i].name + "</p>");
         }
         $("#doctor" + counter).append("<p class='gender'>" + doctor.profile.gender + "</p>");
         $("#doctor" + counter).append("<p class='type'>" + doctor.specialties["0"].actor + "</p>");
         $("#doctor" + counter).append("<p class='category'>" + doctor.specialties["0"].category + "</p>");
-        $("#doctor" + counter).append("<div id='licenses" + counter + "'></div>");
+        $("#doctor" + counter).append("<div id='licenses" + counter + "'><p class='bold'>Licenses:</p></div>");
         for (let x = 0; x < doctor.licenses.length; x++) {
           $("#licenses" + counter).append("<p>" + doctor.licenses[x].number + ' ' + doctor.licenses[x].state + "</p>");
         }
-        $("#doctor" + counter).append("<p class='description'>" + doctor.specialties["0"].description + "</p>");
-
+    
         counter ++;
       });
 
