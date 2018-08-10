63a205992506f9cc71584e7953f9c8098086fa66
Ryan Putman <putman10@me.com>
2018-08-10 09:31:56 -0700

    add dynamic symptom fields

--- src/main.js
+++ src/main.js
@@ -5,10 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  $("#new-symptom").click(function(){
+    $("#symptoms-group").append("<input type='text' name='symptoms' class='form-control' id='symptoms'/>");
+  });
   $("#symptoms-form").submit(function(event){
     event.preventDefault();
     let symptoms = $("#symptoms").val();
+console.log(symptoms);
+    let betterDoctor = new BetterDoctorAPI();  // create instance of BetterDoctor class
+    let returnedDoctorsPromise = betterDoctor.getQualifiedDoctors(symptoms);  // call the instance method and pass in user input
 
-});
-
+    returnedDoctorsPromise.then(function(response) {
+      let body = JSON.parse(response);
+        console.log(body);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 });
--- src/medical-service.js
+++ src/medical-service.js
@@ -2,7 +2,7 @@ export class BetterDoctorAPI{
   getQualifiedDoctors(symptoms) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
