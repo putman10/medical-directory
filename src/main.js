0544ce4c5d3d11744bc21eee91306ca33644bfde
Ryan Putman <putman10@me.com>
2018-08-10 11:00:26 -0700

    add loop for languages

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   });
   $("#symptoms-form").submit(function(event){
     event.preventDefault();
-
+  $("#results").text("");
     let symptoms= $('input[name=symptoms]').map(function(){
       return this.value;
     }).get();
@@ -21,13 +21,20 @@ $(document).ready(function() {
 
     returnedDoctorsPromise.then(function(response) {
       let body = JSON.parse(response);
+      console.log(body);
       let counter = 0;
       body.data.forEach(function(doctor){
         $("#results").append("<div class='col-md-6 doctor-box'id=doctor" + counter + "></div>");
-        $("#doctor" + counter).append(doctor.profile.first_name);
-        $("#doctor" + counter).append(doctor.profile.last_name);
+        $("#doctor" + counter).append("<img src='" + doctor.profile.image_url + "' alt='doctorimage' >");
+        $("#doctor" + counter).append("<p>" + doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title + "</p>");
+        $("#doctor" + counter).append("<div id='language" + counter + "'></div>");
+
+        for (let i = 0; i < doctor.profile.languages.length; i++) {
+          $("#doctor" + counter).append("<p>" + doctor.profile.languages[i].name + "</p>");
+        }
+
         counter ++;
-      })
+      });
 
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
--- src/medical-service.js
+++ src/medical-service.js
@@ -4,8 +4,6 @@ export class BetterDoctorAPI{
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.5122%2C%20-122.6587%2C100&user_location=45.5206%2C-122.6774&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
-      console.log(url);
-
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
