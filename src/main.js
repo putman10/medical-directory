e74e92141e160703229cd114a519bb46e022a038
Ryan Putman <putman10@me.com>
2018-08-10 15:10:15 -0700

    add new medical query to get specialties on load and display in dropdown

--- src/main.js
+++ src/main.js
@@ -1,18 +1,35 @@
 import {BetterDoctorAPI} from './medical-service';
+import {GeocoderAPI} from './geocoder';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  let betterDoctor = new BetterDoctorAPI();
+  let allSpecialties = betterDoctor.getSpecialties();
+
+  allSpecialties.then(function(response) {
+    let body1 = JSON.parse(response);
+    console.log(body1);
+
+    for (let n = 0; n < body1.data.length; n++) {
+      $("#specialties").append("<option value='" + body1.data[n].uid + "'>" + body1.data[n].name + "</option>");
+    }
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    $('.showErrors').show();
+  });
+
   $("#symptoms-form").submit(function(event){
     event.preventDefault();
     $("#results").text("");
+    let specialty = $("#specialties").val();
     let doctorName = $("#name").val();
     let symptoms = $('input[name=symptoms]').val();
-console.log(doctorName);
+    console.log(doctorName);
     let betterDoctor = new BetterDoctorAPI();
-    let returnedDoctorsPromise = betterDoctor.getQualifiedDoctors(symptoms, doctorName);
+    let returnedDoctorsPromise = betterDoctor.getQualifiedDoctors(symptoms, doctorName, specialty);
 
     returnedDoctorsPromise.then(function(response) {
       let body = JSON.parse(response);
@@ -22,31 +39,31 @@ console.log(doctorName);
       if(body.meta.count == 0){
         $("#results").html("<h1 class='noResults'>No Doctors matched your criteria. Try searching again with different search terms.");
       } else {
-          body.data.forEach(function(doctor){
-            $("#results").append("<div class='col-md-3 doctor-box'id=doctor" + counter + "></div>");
-            $("#doctor" + counter).append("<img src='" + doctor.profile.image_url + "' alt='doctorimage' >");
-            $("#doctor" + counter).append("<h3 class='name'>" + doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title + "</h3>");
-            $("#doctor" + counter).append("<p class='address'>" + doctor.practices["0"].visit_address.street + "<br>" + doctor.practices["0"].visit_address.city + ", " + doctor.practices["0"].visit_address.state + " " + doctor.practices["0"].visit_address.zip + "</p>");
-            $("#doctor" + counter).append("<p class='phone'>" + doctor.practices["0"].phones["0"].number + "</p>");
-            if(doctor.practices["0"].accepts_new_patients == true) {
-              $("#doctor" + counter).append("<p class='new-patients'><span class='bold'>Accepting New Patients:</span> Yes</p>");
-            } else {
-              $("#doctor" + counter).append("<p class='new-patients'>Accepting New Patients: Not at this time</p>");
-            }
-            $("#doctor" + counter).append("<p class='description'>" + doctor.specialties["0"].description + "</p>");
-            $("#doctor" + counter).append("<p class='gender'>" + doctor.profile.gender + "</p>");
-            $("#doctor" + counter).append("<p class='type'>" + doctor.specialties["0"].actor + "</p>");
-            $("#doctor" + counter).append("<p class='category'>" + doctor.specialties["0"].category + "</p>");
-            $("#doctor" + counter).append("<div class='languages' id='language" + counter + "'><p class='bold'>Languages:</p></div>");
-            for (let i = 0; i < doctor.profile.languages.length; i++) {
-              $("#language" + counter).append("<p>" + doctor.profile.languages[i].name + "</p>");
-            }
-            $("#doctor" + counter).append("<div class='licenses' id='licenses" + counter + "'><p class='bold'>Licenses:</p></div>");
-            for (let x = 0; x < doctor.licenses.length; x++) {
-              $("#licenses" + counter).append("<p>" + doctor.licenses[x].number + ' ' + doctor.licenses[x].state + "</p>");
-            }
+        body.data.forEach(function(doctor){
+          $("#results").append("<div class='col-md-3 doctor-box'id=doctor" + counter + "></div>");
+          $("#doctor" + counter).append("<img src='" + doctor.profile.image_url + "' alt='doctorimage' >");
+          $("#doctor" + counter).append("<h3 class='name'>" + doctor.profile.first_name + " " + doctor.profile.last_name + " " + doctor.profile.title + "</h3>");
+          $("#doctor" + counter).append("<p class='address'>" + doctor.practices["0"].visit_address.street + "<br>" + doctor.practices["0"].visit_address.city + ", " + doctor.practices["0"].visit_address.state + " " + doctor.practices["0"].visit_address.zip + "</p>");
+          $("#doctor" + counter).append("<p class='phone'>" + doctor.practices["0"].phones["0"].number + "</p>");
+          if(doctor.practices["0"].accepts_new_patients == true) {
+            $("#doctor" + counter).append("<p class='new-patients'><span class='bold'>Accepting New Patients:</span> Yes</p>");
+          } else {
+            $("#doctor" + counter).append("<p class='new-patients'>Accepting New Patients: Not at this time</p>");
+          }
+          $("#doctor" + counter).append("<p class='description'>" + doctor.specialties["0"].description + "</p>");
+          $("#doctor" + counter).append("<p class='gender'>" + doctor.profile.gender + "</p>");
+          $("#doctor" + counter).append("<p class='type'>" + doctor.specialties["0"].actor + "</p>");
+          $("#doctor" + counter).append("<p class='category'>" + doctor.specialties["0"].category + "</p>");
+          $("#doctor" + counter).append("<div class='languages' id='language" + counter + "'><p class='bold'>Languages:</p></div>");
+          for (let i = 0; i < doctor.profile.languages.length; i++) {
+            $("#language" + counter).append("<p>" + doctor.profile.languages[i].name + "</p>");
+          }
+          $("#doctor" + counter).append("<div class='licenses' id='licenses" + counter + "'><p class='bold'>Licenses:</p></div>");
+          for (let x = 0; x < doctor.licenses.length; x++) {
+            $("#licenses" + counter).append("<p>" + doctor.licenses[x].number + ' ' + doctor.licenses[x].state + "</p>");
+          }
 
-            counter ++;
+          counter ++;
         });
       }
 
--- src/medical-service.js
+++ src/medical-service.js
@@ -1,9 +1,26 @@
 export class BetterDoctorAPI{
-  getQualifiedDoctors(symptoms, searchName) {
+  getQualifiedDoctors(symptoms, searchName, specialty) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.5122%2C%20-122.6587%2C100&name=${searchName}&user_location=45.5206%2C-122.6774&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-console.log(url);
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&specialty_uid=${specialty}&location=45.5122%2C%20-122.6587%2C100&name=${searchName}&user_location=45.5206%2C-122.6774&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      console.log(url);
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getSpecialties() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=${process.env.exports.apiKey}`;
+
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
