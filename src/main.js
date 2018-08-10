d5a19ee4e204cd43db613809fab91179f9ca2531
Ryan Putman <putman10@me.com>
2018-08-10 10:17:11 -0700

    add loop for individual returned search results

--- src/main.js
+++ src/main.js
@@ -5,24 +5,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $("#new-symptom").click(function(){
-    $("#symptoms-group").append("<input type='text' name='symptoms' class='form-control' id='symptoms'/>");
+  $("#new-symptom").click(function(event){
+    event.preventDefault();
+    $("#symptoms-group").append("<input type='text' name='symptoms' class='form-control'/>");
   });
   $("#symptoms-form").submit(function(event){
     event.preventDefault();
 
-    let symptoms= $('input[id=symptoms]').map(function(){
+    let symptoms= $('input[name=symptoms]').map(function(){
       return this.value;
     }).get();
 
-    let betterDoctor = new BetterDoctorAPI();  // create instance of BetterDoctor class
-    let returnedDoctorsPromise = betterDoctor.getQualifiedDoctors(symptoms);  // call the instance method and pass in user input
+    let betterDoctor = new BetterDoctorAPI();
+    let returnedDoctorsPromise = betterDoctor.getQualifiedDoctors(symptoms);
 
     returnedDoctorsPromise.then(function(response) {
       let body = JSON.parse(response);
         console.log(body);
+
+        body.data.forEach(function(doctor){
+          $("#results").append(doctor.profile.first_name);
+        })
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
+
+
   });
 });
--- src/medical-service.js
+++ src/medical-service.js
@@ -2,7 +2,9 @@ export class BetterDoctorAPI{
   getQualifiedDoctors(symptoms) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.5122%2C%20-122.6587%2C100&user_location=45.5206%2C-122.6774&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
+      console.log(url);
 
       request.onload = function() {
         if (this.status === 200) {
